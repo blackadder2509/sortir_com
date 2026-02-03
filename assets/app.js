02e01a4d57f821dde697179899dd06faa016b1db
blackadder2509 <baynardanabelle@gmail.com>
2026-02-03 15:47:26 +0100

    modification .gitignore

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,8 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'bootstrap';  // Déclaration du JS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';  // Déclaration du CSS de Bootstrap
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
