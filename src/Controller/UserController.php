466139dfc397c52d47bfb2d5bd140efc948b4cd5
blackadder2509 <baynardanabelle@gmail.com>
2026-02-09 14:39:14 +0100

    ajout des routes pour modifier son profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Entity\User; // Un seul import pour l'entité User
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,10 +11,53 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user')]
 final class UserController extends AbstractController
 {
+    /**
+     * PROFIL : Permet à l'utilisateur connecté de modifier ses propres infos
+     * On utilise 'priority' pour que cette route passe avant /{id}
+     */
+    #[Route('/mon-profil', name: 'app_user_profile_edit', methods: ['GET', 'POST'], priority: 10)]
+    public function profileEdit(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        /** @var User $user */
+        $user = $this->getUser(); // Récupère l'utilisateur en session
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Gestion du mot de passe via le champ non-mappé 'plainPassword'
+            $plainPassword = $form->get('plainPassword')->getData();
+            if ($plainPassword) {
+                $user->setPassword($passwordHasher->hashPassword($user, $plainPassword));
+            }
+
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre profil a été mis à jour.');
+            return $this->redirectToRoute('app_user_show', ['id' => $user->getId()]);
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * ADMIN : Liste des utilisateurs
+     */
+    #[IsGranted('ROLE_ADMIN')]
     #[Route(name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
@@ -23,19 +66,29 @@ final class UserController extends AbstractController
         ]);
     }
 
+    /**
+     * ADMIN : Créer un nouvel utilisateur
+     */
+    //#[IsGranted('ROLE_ADMIN')]
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, $passwordHasher): Response
-    {
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher // <--- AJOUTE LE TYPE ICI
+    ): Response {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $hashedPassword = $passwordHasher->hashPassword(
-                $user,
-                $user->getPassword()
-            );
-            $user->setPassword($hashedPassword);
+            // On récupère le mot de passe depuis le champ plainPassword du formulaire
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            if ($plainPassword) {
+                $user->setPassword(
+                    $passwordHasher->hashPassword($user, $plainPassword)
+                );
+            }
 
             $entityManager->persist($user);
             $entityManager->flush();
@@ -48,8 +101,11 @@ final class UserController extends AbstractController
             'form' => $form,
         ]);
     }
-    
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+
+    /**
+     * PUBLIC/CONNECTÉ : Voir un profil par son ID
+     */
+    #[Route('/{id}', name: 'app_user_show', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function show(User $user): Response
     {
         return $this->render('user/show.html.twig', [
@@ -57,24 +113,10 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
+    /**
+     * ADMIN : Supprimer un utilisateur
+     */
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
