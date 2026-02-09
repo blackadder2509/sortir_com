b5f44cd6061b08c01bc13eae041085a415c0e787
blackadder2509 <baynardanabelle@gmail.com>
2026-02-09 14:02:18 +0100

    login avec email et pseudo

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -48,7 +48,7 @@ final class UserController extends AbstractController
             'form' => $form,
         ]);
     }
-
+    
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
